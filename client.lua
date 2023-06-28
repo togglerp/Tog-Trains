6646f46e5066dbf85bb5163fdb6d873fe2f7cc99
Skunk <134791609+SkunkDevelopment@users.noreply.github.com>
2023-06-28 01:45:55 +0100

    Added Config

--- client.lua
+++ client.lua
@@ -1,5 +1,9 @@
-SwitchTrainTrack(0, true) -- Setting the Main train track(s) around LS and towards Sandy Shores active
-SwitchTrainTrack(3, true) -- Setting the Metro tracks active
-SetTrainTrackSpawnFrequency(0, 120000) -- The Train spawn frequency set for the game engine
-SetTrainTrackSpawnFrequency(3, 120000) -- The Metro spawn frequency set for the game engine
-SetRandomTrains(true) -- Telling the game we want to use randomly spawned trains
+if Config.EnableTrains then
+  SwitchTrainTrack(0, true) -- Setting the Main train track(s) around LS and towards Sandy Shores active
+  SwitchTrainTrack(3, true) -- Setting the Metro tracks active
+  SetTrainTrackSpawnFrequency(0, 120000) -- The Train spawn frequency set for the game engine
+  SetTrainTrackSpawnFrequency(3, 120000) -- The Metro spawn frequency set for the game engine
+  SetRandomTrains(true) -- Telling the game we want to use randomly spawned trains
+else
+  print ('Trains are not enabled, Please view the Config file to enable them')
+end
